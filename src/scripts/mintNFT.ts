4a0a7f8d47a611d94db9f296f96903a2137d7b7f
Samson Ajulor <samsonajulor@gmail.com>
2023-09-12 03:39:37 +0100

    updated the contract address

--- src/scripts/mintNFT.ts
+++ src/scripts/mintNFT.ts
@@ -16,7 +16,6 @@ async function main() {
     signer.address,
     'bafyreiblrs3ktv36q3o2ssatubyfndoo4z3dg3agn3qpgdsmuao7a5sqoy'
   );
-  // const setTokenData = await NFTProtoContract.mintNFT(signer.address, storedNFTOnIPFS.url);
   // @ts-ignore
   const setTokenDataLog = (await setTokenData.wait())?.logs;
   console.log(setTokenDataLog);
